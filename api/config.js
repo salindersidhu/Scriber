00178280e40bcdfdfcf68094205a9671a78a7f23
Salinder Sidhu <salinder.sid@gmail.com>
2019-01-20 00:55:36 -0500

    Fixed bug with config.js

--- api/config.js
+++ api/config.js
@@ -1,10 +1,6 @@
 const crypto = require('crypto');
 const config = require('./config.json')[process.env.NODE_ENV || 'development'];
 
-const extended = {
-    session: {
-        key: crypto.randomBytes(256).toString('hex')
-    }
-};
+config.session.key = crypto.randomBytes(256).toString('hex');
 
-module.exports = { ...config, ...extended };
+module.exports = config;
