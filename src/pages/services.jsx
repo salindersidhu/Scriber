f24af220d18a734009e0f6d78d9c200a38336745
Salinder Sidhu <salinder.sid@gmail.com>
2019-03-09 17:37:31 -0500

    Updated services base url

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const apiVersion1Url = 'http://localhost:8000/api/v1';
+const apiBaseUrl = 'http://localhost:8000/api/v1';
 
 // API service calls
 const Services = {
     CreateUser: (data) => {
-        return axios.post(apiVersion1Url + '/users', data);
+        return axios.post(apiBaseUrl + '/users', data);
     },
     CreateUserSession: (data) => {
-        return axios.post(apiVersion1Url + '/sessions', data);
+        return axios.post(apiBaseUrl + '/sessions', data);
     }
 };
 
