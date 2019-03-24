3cb7e315880d1f5355df46146a5aef9d34f1cb3e
Salinder Sidhu <salinder.sid@gmail.com>
2019-03-24 02:46:16 -0400

    Implemented api tests

--- api/tests/users.test.js
+++ api/tests/users.test.js
@@ -1,5 +1,17 @@
-describe('User API', () => {
-    it('should pass', () => {
-        expect(true);
+const express = require('express');
+const request = require('supertest');
+
+/* Import server app */
+const app = express();
+const router = express.Router();
+const routes = require('../routes');
+
+/* Configure server app */
+routes(router);
+app.use('/api/v1', router);
+
+describe('Test User API', () => {
+    test('It should response the GET method', () => {
+        return request(app).get('/api/v1/users').expect(404);
     });
 });
